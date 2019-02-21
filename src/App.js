8db90a9fa7121257823add64660d19e0d98415ad
Joni Mettälä <joni.mettala@gmail.com>
2019-02-21 21:48:57 +0200

    Add navigation bar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
 import Button from '@material-ui/core/Button';
+import NavBar from './components/NavBar';
 
 class App extends Component {
   render() {
     return (
-      <Button variant="contained" color="primary">
-        Material Button
-      </Button>
+      <div>
+        <NavBar />
+        <Button variant="contained" color="primary">
+          Material Button
+        </Button>
+      </div>
     );
   }
 }
