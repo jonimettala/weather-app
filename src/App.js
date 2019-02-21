77509d44efff45731e3a5a40811ec180ea5e55bc
Joni Mettälä <joni.mettala@gmail.com>
2019-02-21 21:59:00 +0200

    Add search field

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
 import React, { Component } from 'react';
 import Button from '@material-ui/core/Button';
 import NavBar from './components/NavBar';
+import SearchField from './components/SearchField';
 
 class App extends Component {
   render() {
     return (
       <div>
         <NavBar />
-        <Button variant="contained" color="primary">
-          Material Button
-        </Button>
+        <SearchField />
       </div>
     );
   }
