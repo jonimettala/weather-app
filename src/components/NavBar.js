72b0e86206a609aeea26e363a633f0c5996c79d0
Joni Mettälä <joni.mettala@gmail.com>
2019-02-28 14:05:50 +0200

    Add GitHub button

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,17 +2,36 @@ import React from 'react'
 import AppBar from '@material-ui/core/AppBar'
 import Toolbar from '@material-ui/core/Toolbar'
 import Typography from '@material-ui/core/Typography'
-const NavBar = () => {
-    return(
-        <div>
-        <AppBar position="static">
-            <Toolbar>
-                <Typography variant="title" color="inherit">
-                Weather Now
-                </Typography>
-            </Toolbar>
-        </AppBar>
-        </div>
-    )
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
+
+const styles = {
+  root: {
+    flexGrow: 1,
+  },
+  grow: {
+    flexGrow: 1,
+  },
+};
+
+const NavBar = (props) => {
+  const { classes } = props;
+
+  return(
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="title" color="inherit" className={classes.grow}>
+            Weather Now
+          </Typography>
+          <Button color="inherit" href="https://github.com/jonimettala/weather-app">Fork on GitHub</Button>
+        </Toolbar>
+      </AppBar>
+  )
 }
-export default NavBar;
+
+NavBar.propTypes = {
+    classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(NavBar);
